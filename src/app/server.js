80d43b1b9ff112b007a9f7cb13f900f3b7ad6248
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-25 17:51:48 -0600

    Añadir exportación de la constante "app" y la importación de "cors" en server.js y index.js

--- src/app/server.js
+++ src/app/server.js
@@ -1,8 +1,3 @@
 import express from 'express';
-import cors from 'cors';
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.listen(3000);
+export const app = express();
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { app } from './app/server.js';
+import { config } from 'dotenv';
+import cors from 'cors';
