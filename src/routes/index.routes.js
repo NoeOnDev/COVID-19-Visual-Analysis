19c53c78cb32dffbb568943d052876d9801b6fd4
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-05-11 16:27:25 -0600

    Eliminar archivos y código innecesarios

--- src/routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import userRoutes from './user.routes.js';
-
-const indexRouter = Router();
-
-indexRouter.use('/user', userRoutes);
-
-export default indexRouter;
--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { UserController } from '../controller/user.controller.js';
-
-const router = Router();
-
-router.post('/register', UserController.registerUser);
-router.get('/verify/:token', UserController.verifyUser);
-
-export default router;
