6e5ca1e7f2a64739caa2abde8e539c52d6acf1f8
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-05-11 21:22:30 -0600

    chore: Actualizar gráfico de casos y defunciones en varios países

--- axis-covid-19-cases-2020-2024/3.py
+++ axis-covid-19-cases-2020-2024/3.py
@@ -31,6 +31,7 @@ for pais in paises:
     totales_infecciones.append(total_infecciones)
     totales_muertes.append(total_muertes)
 
+# Gráfico vertical
 x = range(len(paises))
 
 plt.figure(figsize=(12, 6))
@@ -40,8 +41,24 @@ plt.bar(x, totales_muertes, width=0.4, label='Muertes', color='r', align='edge')
 
 plt.xlabel('País')
 plt.ylabel('Total')
-plt.title('Total de infecciones y muertes por COVID por país')
+plt.title('Total de infecciones y muertes por COVID por país (Vertical)')
 plt.xticks(x, paises, rotation=90)
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+# Gráfico horizontal
+y = range(len(paises))
+
+plt.figure(figsize=(12, 6))
+
+plt.barh(y, totales_infecciones, height=0.4, label='Infecciones', color='b', align='edge')
+plt.barh(y, totales_muertes, height=0.4, label='Muertes', color='r', align='center')
+
+plt.ylabel('País')
+plt.xlabel('Total')
+plt.title('Total de infecciones y muertes por COVID por país (Horizontal)')
+plt.yticks(y, paises)
 plt.legend()
 plt.tight_layout()
 plt.show()
